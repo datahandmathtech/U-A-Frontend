4aaf518a30043fc4adab2a0fae1a8244777b24db
Abhaypraja09 <abhaypraja09@gmail.com>
2026-08-31 18:45:23 +0530

    Fix missing action cell for edit/delete

--- src/pages/InventoryLedger.tsx
+++ src/pages/InventoryLedger.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { 
-  Box, Typography, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, 
+  Box, IconButton, Typography, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, 
   Button, Dialog, DialogTitle, DialogContent, DialogActions, TextField, Checkbox, FormControlLabel,
   MenuItem, Select, InputLabel, FormControl, Autocomplete
 } from '@mui/material';
