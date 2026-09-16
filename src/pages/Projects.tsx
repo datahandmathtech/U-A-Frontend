f69afadc1fadbbe2b1459f16e64dc7180d374889
Abhaypraja09 <abhaypraja09@gmail.com>
2026-09-16 14:40:59 +0530

    build: update production build in dist and fix Projects.tsx warning

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1010,7 +1010,7 @@ const Projects: React.FC = () => {
                           </IconButton>
                         )}
 
-                        <Grid container spacing={1.5} alignItems="center">
+                        <Grid container spacing={1.5} sx={{ alignItems: 'center' }}>
                           <Grid size={{ xs: 12, sm: 4 }}>
                             <TextField
                               label="Product / Material Name"
